5036d7b6adbfa8c26500a3b4e68d726f89ad3a58
euzl <euzl@khu.ac.kr>
2020-07-10 00:01:17 +0900

    feat:todo추가

--- app/src/main/java/com/dasom/khuhelper/MainActivity.java
+++ app/src/main/java/com/dasom/khuhelper/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void initView() {
         LinearLayout searchBtn = findViewById(R.id.btn_search_ev_place);
         searchBtn.setOnClickListener(this);
+        // TODO: 09/07/2020 지도api 연결 & 충전소 위치 띄우기 
     }
 
     @Override
