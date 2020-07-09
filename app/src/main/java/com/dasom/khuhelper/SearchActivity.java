76010ac0be2df47dacb27615e75c6e033f6113b0
euzl <euzl@khu.ac.kr>
2020-07-10 00:31:00 +0900

    feat:레이아웃 구성, 키보드 띄워놓고 시작
    뒤로가기 버튼 : 현 액티비티 종료
    initView : 아이디 하나씩 다 지정
    imm : 키보드 올라와있게 설정

--- app/src/main/java/com/dasom/khuhelper/SearchActivity.java
+++ app/src/main/java/com/dasom/khuhelper/SearchActivity.java
@@ -3,7 +3,10 @@ package com.dasom.khuhelper;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,7 +15,7 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_search);
 
         initView();
     }
@@ -21,7 +24,7 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_search_back:
-                startActivity(new Intent(SearchActivity.this, MainActivity.class));
+                finish();
                 break;
             case R.id.btn_search:
                 // TODO: 09/07/2020 '검색'버튼 눌렀을 때 액션 (검색내용에 맞는 결과 띄워주기)
@@ -30,10 +33,16 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
     }
 
     private void initView() {
-        Button userLoginBtn = findViewById(R.id.btn_user_login);
-        Button adminLoginBtn = findViewById(R.id.btn_admin_login);
+        ImageView userLoginBtn = findViewById(R.id.btn_search_back);
+        EditText searchEdt = findViewById(R.id.edt_ev_search);
+        Button adminLoginBtn = findViewById(R.id.btn_search);
+
+        searchEdt.requestFocus();
 
         userLoginBtn.setOnClickListener(this);
         adminLoginBtn.setOnClickListener(this);
+
+        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED, InputMethodManager.HIDE_IMPLICIT_ONLY);
     }
 }
