b5e7e5949124604224914250aac156c10082d2e4
euzl <euzl@khu.ac.kr>
2020-07-09 23:10:15 +0900

    feat:사용자 메인화면 클래스 추가

--- app/src/main/java/com/dasom/khuhelper/MainActivity.java
+++ app/src/main/java/com/dasom/khuhelper/MainActivity.java
@@ -2,13 +2,32 @@ package com.dasom.khuhelper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        LinearLayout searchBtn = findViewById(R.id.btn_search_ev_place);
+        searchBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.btn_search_ev_place:
+                startActivity(new Intent(MainActivity.this, SearchActivity.class));
+                break;
+        }
     }
 }
