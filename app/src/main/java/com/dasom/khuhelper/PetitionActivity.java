08a4f7bb0adba38f4db5971a16903094d8da636a
euzl <euzl@khu.ac.kr>
2020-07-14 20:54:31 +0900

    feat:충전소 민원접수 액티비티 클래스 생성

---
+++ app/src/main/java/com/dasom/khuhelper/PetitionActivity.java
@@ -0,0 +1,43 @@
+package com.dasom.khuhelper;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PetitionActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_petition);
+
+        initView();
+    }
+
+    private void initView() {
+        Button petitionSubmitBtn = findViewById(R.id.btn_petition_submit);
+        ImageView searchBackBtn = findViewById(R.id.btn_search_back);
+        petitionSubmitBtn.setOnClickListener(this);
+        searchBackBtn.setOnClickListener(this);
+
+        // TODO: 14/07/2020 충전소 이름이 표시되도록 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.btn_petition_submit:
+                // TODO: 14/07/2020 DB에 관련 내용들이 저장되도록 구현
+                Toast.makeText(this.getApplicationContext(),R.string.cp_submit_success, Toast.LENGTH_SHORT).show();
+                finish()
+                break;
+            case R.id.btn_search_back:
+                finish();
+                break;
+        }
+    }
+}
