d5c1b75b398bbe930455b2e11884ed746498382e
euzl <euzl@khu.ac.kr>
2020-07-20 02:38:39 +0900

    fix:네이티브 앱 키 변수명 수정
    Related : #8

--- app/src/main/java/com/dasom/khuhelper/MapApiConst.java
+++ app/src/main/java/com/dasom/khuhelper/MapApiConst.java
@@ -2,6 +2,6 @@ package com.dasom.khuhelper;
 
 public class MapApiConst {
     // http://developers.daum.net/console 네이티브 앱 키
-    public static final String DAUM_MAPS_ANDROID_APP_API_KEY = "6f504f9b73ad280372b2aff0036b6f32";
+    public static final String KAKAO_NATIVE_APP_API_KEY = "6f504f9b73ad280372b2aff0036b6f32";
 }
 
