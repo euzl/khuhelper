065599ff5b3396a9324870cf97f7defb9649ea83
euzl <euzl@khu.ac.kr>
2020-08-03 22:21:41 +0900

    feat: 다중 마커 띄우기
    Related #9

--- app/src/main/java/com/dasom/khuhelper/MainActivity.java
+++ app/src/main/java/com/dasom/khuhelper/MainActivity.java
@@ -42,6 +42,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mapPOIItem.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
         mapPOIItem.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
         mapView.addPOIItem(mapPOIItem);
+
+        MapPOIItem mapPOIItem2 = new MapPOIItem();
+        mapPOIItem2.setItemName("덕수궁");
+        mapPOIItem2.setTag(1);
+        mapPOIItem2.setMapPoint(MapPoint.mapPointWithGeoCoord(37.565984, 126.975570));
+        mapPOIItem2.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
+        mapPOIItem2.setCustomImageResourceId(R.drawable.ic_ev_place); // 마커 이미지.
+        mapPOIItem2.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+        mapPOIItem2.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+        mapView.addPOIItem(mapPOIItem2);
     }
 
     @Override
