395e491a01b57022a51e39e0b15d4d0531072436
euzl <euzl@khu.ac.kr>
2020-08-04 01:58:27 +0900

    feat: 충전소 정보 저장 객체 생성
    하긴 했는데... 더 좋은 방법이 있을 것 같다..
    메모리 낭비가 어마어마하다..
    
    Related #9

--- app/src/main/java/com/dasom/khuhelper/ChargingStation.java
+++ app/src/main/java/com/dasom/khuhelper/ChargingStation.java
@@ -2,4 +2,67 @@ package com.dasom.khuhelper;
 
 public class ChargingStation {
 
+    private Integer statId; // 충전소ID (고유값)
+    private String statNm; // 충전소 이름
+    private Integer chgerType; // 충전기 타입
+    private String addrDoro; // 도로명주소
+    private Integer lat; // 위도
+    private Integer lng; // 경도
+    private String useTime; // 이용시간
+
+    public void setStatId(Integer statId) {
+        this.statId = statId;
+    }
+
+    public void setStatNm(String statNm) {
+        this.statNm = statNm;
+    }
+
+    public void setChgerType(Integer chgerType) {
+        this.chgerType = chgerType;
+    }
+
+    public void setAddrDoro(String addrDoro) {
+        this.addrDoro = addrDoro;
+    }
+
+    public void setLat(Integer lat) {
+        this.lat = lat;
+    }
+
+    public void setLng(Integer lng) {
+        this.lng = lng;
+    }
+
+    public void setUseTime(String useTime) {
+        this.useTime = useTime;
+    }
+
+    public Integer getStatId() {
+        return statId;
+    }
+
+    public String getStatNm() {
+        return statNm;
+    }
+
+    public Integer getChgerType() {
+        return chgerType;
+    }
+
+    public String getAddrDoro() {
+        return addrDoro;
+    }
+
+    public Integer getLat() {
+        return lat;
+    }
+
+    public Integer getLng() {
+        return lng;
+    }
+
+    public String getUseTime() {
+        return useTime;
+    }
 }
