c7a5ee6bd64bcc3742cce36a345f1a006390df70
euzl <euzl@khu.ac.kr>
2020-08-05 01:54:40 +0900

    feat: api 파싱해서 얻은 충전소 마커 표시
    충전소(arraylist에 저장) -> callback 으로 얻어옴
    
    감.동.적
    
    Related #9

--- app/src/main/java/com/dasom/khuhelper/MainActivity.java
+++ app/src/main/java/com/dasom/khuhelper/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
@@ -12,21 +13,23 @@ import net.daum.mf.map.api.MapPOIItem;
 import net.daum.mf.map.api.MapPoint;
 import net.daum.mf.map.api.MapView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     LinearLayout searchBtn;
     ViewGroup mapViewContainer;
     MapView mapView;
 
+    ChargingStationParser chargingStationParser;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        parseChargingStation(); // 충전소 api 파싱
-
         initView();
-        markChargingStation(); // 마커 표시
+        parseChargingStation(); // 충전소 api 파싱
     }
 
     @Override
@@ -52,20 +55,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void parseChargingStation() {
         // 충전소 파서 실행 (AsyncTask)
-        ChargingStationParser chargingStationParser = new ChargingStationParser();
+        chargingStationParser = new ChargingStationParser(new ChargingStationParserCallBack() {
+            @Override
+            public void onSuccess(ArrayList<ChargingStation> chargingStations) {
+                markChargingStation(chargingStations);
+            }
+        });
         chargingStationParser.execute();
     }
 
-    private void markChargingStation() {
-        // 마커표시 테스트
-//        MapPOIItem mapPOIItem = new MapPOIItem();
-//        mapPOIItem.setItemName("서울시청");
-//        mapPOIItem.setTag(0);
-//        mapPOIItem.setMapPoint(MapPoint.mapPointWithGeoCoord(37.5666805, 126.9784147));
-//        mapPOIItem.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
-//        mapPOIItem.setCustomImageResourceId(R.drawable.ic_ev_place); // 마커 이미지.
-//        mapPOIItem.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
-//        mapPOIItem.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
-//        mapView.addPOIItem(mapPOIItem);
+    private void markChargingStation(ArrayList<ChargingStation> chargingStations) {
+        MapPOIItem mapPOIItem;
+        Log.d("Mark Carging Station", "마커표시시작");
+        for (ChargingStation cs : chargingStations) {
+            mapPOIItem = new MapPOIItem();
+            mapPOIItem.setItemName(cs.getStatNm());
+//            mapPOIItem.setTag(cs.getStatId()); // 일단 생략..
+            mapPOIItem.setMapPoint(MapPoint.mapPointWithGeoCoord(cs.getLat(), cs.getLng()));
+            mapPOIItem.setMarkerType(MapPOIItem.MarkerType.CustomImage); // 마커타입을 커스텀 마커로 지정.
+            mapPOIItem.setCustomImageResourceId(R.drawable.ic_ev_place); // 마커 이미지.
+            mapPOIItem.setCustomImageAutoscale(false); // hdpi, xhdpi 등 안드로이드 플랫폼의 스케일을 사용할 경우 지도 라이브러리의 스케일 기능을 꺼줌.
+            mapPOIItem.setCustomImageAnchor(0.5f, 1.0f); // 마커 이미지중 기준이 되는 위치(앵커포인트) 지정 - 마커 이미지 좌측 상단 기준 x(0.0f ~ 1.0f), y(0.0f ~ 1.0f) 값.
+            mapView.addPOIItem(mapPOIItem);
+        }
+        // TODO: 05/08/2020 마커를 클릭하면 에러남 ㅠ  
     }
 }
