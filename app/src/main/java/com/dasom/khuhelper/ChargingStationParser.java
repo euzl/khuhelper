c5858a690f5b7cb8c47b1467963371b0ff2f5601
euzl <euzl@khu.ac.kr>
2020-08-05 01:14:44 +0900

    feat: 충전소 파싱해서 어레이리스트에 저장
    Related #9

--- app/src/main/java/com/dasom/khuhelper/ChargingStationParser.java
+++ app/src/main/java/com/dasom/khuhelper/ChargingStationParser.java
@@ -11,83 +11,60 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
 
-public class ChargingStationParser extends AsyncTask<String, Integer, String> {
+public class ChargingStationParser extends AsyncTask<Void, Void, Void> {
 
-    private String queryURL = "http://open.ev.or.kr:8080/openapi/services/rest/EvChargerService?"//요청 URL
-            +"serviceKey=" + ServiceKey.key;
+    private String queryURL;
 
-    ArrayList<ChargingStation> csList = new ArrayList<>();
+    private ArrayList<ChargingStation> csList;
+    private ChargingStationParserCallBack cspCallBack;
+
+    private int eventType;
+    private String tag;
+    private XmlPullParserFactory factory;
+    private XmlPullParser xpp;
+
+    public ChargingStationParser(ChargingStationParserCallBack cspCallBack) {
+        this.cspCallBack = cspCallBack;
+        csList = new ArrayList<>();
+        queryURL = "http://open.ev.or.kr:8080/openapi/services/rest/EvChargerService?"//요청 URL
+                +"serviceKey=" + ServiceKey.key;
+    }
 
     @Override
-    protected String doInBackground(String... strings) {
+    protected Void doInBackground(Void... params) {
         try {
-            URL url= new URL(queryURL);//문자열로 된 요청 url을 URL 객체로 생성.
-            InputStream is= url.openStream(); //url위치로 입력스트림 연결
+            URL url = new URL(queryURL); //문자열로 된 요청 url을 URL 객체로 생성.
+            InputStream is = url.openStream(); //url위치로 입력스트림 연결
 
-            XmlPullParserFactory factory= XmlPullParserFactory.newInstance();
-            XmlPullParser xpp= factory.newPullParser();
+            factory = XmlPullParserFactory.newInstance();
+            xpp = factory.newPullParser();
             xpp.setInput( new InputStreamReader(is, "UTF-8") ); //inputstream 으로부터 xml 입력받기
 
-            String tag;
-
             xpp.next();
-            int eventType= xpp.getEventType();
-
-            ChargingStation chargingStation = new ChargingStation();
+            eventType = xpp.getEventType();
 
             while( eventType != XmlPullParser.END_DOCUMENT ){
                 switch( eventType ){
                     case XmlPullParser.START_DOCUMENT:
                         Log.d("Parser", "파싱 시작");
-                        chargingStation = new ChargingStation(); // 얘 위치가 어디가 적합할지 생각해보기
                         break;
 
                     case XmlPullParser.START_TAG: // 태그 내용 chargingStation에 저장
                         tag = xpp.getName(); //태그 이름 얻어오기
-
-                        if(tag.equals("item")) ; // 첫번째 검색결과
-                        else if(tag.equals("statId")){
-                            xpp.next();
-                            chargingStation.setStatId(Integer.parseInt(xpp.getText()));
-                        }
-                        else if(tag.equals("statNm")){
-                            xpp.next();
-                            chargingStation.setStatNm(xpp.getText());
-                        }
-                        else if(tag.equals("chgerType")){
-                            xpp.next();
-                            chargingStation.setChgerType(Integer.parseInt(xpp.getText()));
-                        }
-                        else if(tag.equals("addrDoro")){
-                            xpp.next();
-                            chargingStation.setAddrDoro(xpp.getText());
-                        }
-                        else if(tag.equals("lat")){
-                            xpp.next();
-                            chargingStation.setLat(Integer.parseInt(xpp.getText()));
-                        }
-                        else if(tag.equals("lng")){
-                            xpp.next();
-                            chargingStation.setLng(Integer.parseInt(xpp.getText()));
-                        }
-                        else if(tag.equals("useTime")){
-                            xpp.next();
-                            chargingStation.setUseTime(xpp.getText());
-                        }
+                        csList.add(setChargingStation());
                         break;
 
                     case XmlPullParser.TEXT:
                         break;
 
                     case XmlPullParser.END_TAG:
-                        csList.add(chargingStation); // 리스트에 추가
+                        Log.d("Parser", "END_TAG");
                         break;
                 }
-                eventType= xpp.next();
+                eventType = xpp.next();
             }
         } catch (Exception e) {
             System.err.println("There was an error: " + e.getCause() + " : " + e.getMessage());
-            // TODO Auto-generated catch blocke.printStackTrace();
         }
 
         Log.d("Parser", "파싱 끝...");
@@ -96,7 +73,69 @@ public class ChargingStationParser extends AsyncTask<String, Integer, String> {
     }
 
     @Override
-    protected void onPostExecute(String str) {
-        super.onPostExecute(str);
+    protected void onPostExecute(Void vd) {
+        super.onPostExecute(vd);
+        cspCallBack.onSuccess(csList);
+    }
+
+    private ChargingStation setChargingStation() {
+        ChargingStation tmpStation = new ChargingStation();
+        boolean isEndTag = false;
+
+        try {
+            while( eventType != XmlPullParser.END_DOCUMENT ){
+                switch( eventType ){
+                    default:
+                        isEndTag = false;
+                        break;
+
+                    case XmlPullParser.START_TAG:
+                        isEndTag = false;
+                        tag = xpp.getName(); //태그 이름 얻어오기
+
+                        if(tag.equals("item")) {
+                        }
+                        else if(tag.equals("statId")){
+                            xpp.next();
+                            tmpStation.setStatId(xpp.getText());
+                        }
+                        else if(tag.equals("statNm")){
+                            xpp.next();
+                            tmpStation.setStatNm(xpp.getText());
+                        }
+                        else if(tag.equals("chgerType")){
+                            xpp.next();
+                            tmpStation.setChgerType(Integer.parseInt(xpp.getText()));
+                        }
+                        else if(tag.equals("addrDoro")){
+                            xpp.next();
+                            tmpStation.setAddrDoro(xpp.getText());
+                        }
+                        else if(tag.equals("lat")){
+                            xpp.next();
+                            tmpStation.setLat(Float.parseFloat(xpp.getText()));
+                        }
+                        else if(tag.equals("lng")){
+                            xpp.next();
+                            tmpStation.setLng(Float.parseFloat(xpp.getText()));
+                        }
+                        else if(tag.equals("useTime")){
+                            xpp.next();
+                            tmpStation.setUseTime(xpp.getText());
+                        }
+
+                        break;
+
+                    case XmlPullParser.END_TAG:
+                        if (isEndTag) return tmpStation;
+                        isEndTag = true;
+                }
+                eventType = xpp.next();
+            }
+        } catch (Exception e) {
+            System.err.println("There was an error: " + e.getCause() + " : " + e.getMessage());
+        }
+
+        return tmpStation;
     }
 }
