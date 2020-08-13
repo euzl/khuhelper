2a539137027069723cd26467fada65440d25d9ec
euzl <euzl@khu.ac.kr>
2020-08-14 08:50:46 +0900

    feat: 최적지 지도에 표시

--- app/src/main/java/com/dasom/khuhelper/admin/AnalyzePlace.java
+++ app/src/main/java/com/dasom/khuhelper/admin/AnalyzePlace.java
@@ -1,18 +1,8 @@
 package com.dasom.khuhelper.admin;
 
-import android.graphics.Point;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-
 public class AnalyzePlace {
     private int id; // 아이디
     private double finalPoint; // 최종포인트(높을수록 추천)
-    private ArrayList<Float> latitudes; // 위도
-    private ArrayList<Float> longtitudes; // 경도
     private double[][] points;
 
     public int getId() {
@@ -29,22 +19,6 @@ public class AnalyzePlace {
 
     public void setFinalPoint(double finalPoint) {
         this.finalPoint = finalPoint;
-    }
-
-    public ArrayList<Float> getlatitudes() {
-        return latitudes;
-    }
-
-    public void setlatitudes(ArrayList<Float> latitudes) {
-        this.latitudes = latitudes;
-    }
-
-    public ArrayList<Float> getlongtitudes() {
-        return longtitudes;
-    }
-
-    public void setlongtitudes(ArrayList<Float> longtitudes) {
-        this.longtitudes = longtitudes;
     }
 
     public double[][] getPoints() {
--- app/src/main/java/com/dasom/khuhelper/admin/OptimalPlaceActivity.java
+++ app/src/main/java/com/dasom/khuhelper/admin/OptimalPlaceActivity.java
@@ -1,11 +1,15 @@
 package com.dasom.khuhelper.admin;
 
 import android.content.res.AssetManager;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.dasom.khuhelper.R;
 
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapPolyline;
 import net.daum.mf.map.api.MapView;
 
 import org.json.JSONArray;
@@ -23,33 +27,53 @@ public class OptimalPlaceActivity extends AppCompatActivity {
     ViewGroup mapViewContainer;
     MapView mapView;
 
-    ArrayList<AnalyzePlace> analyzePlaces;
+    TextView testTv;
+
+    ArrayList<AnalyzePlace> analyzePlaces = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_optimalplace);
 
-//        initView();
+        initView();
         setAnalyzePlaces();
+        setPolyLine();
     }
 
     private void initView() {
+        testTv = findViewById(R.id.tv_parsingtest);
+
         mapViewContainer = findViewById(R.id.map_view);
         mapView = new MapView(this);
         mapViewContainer.addView(mapView);
     }
 
+    private void setPolyLine() {
+        for (AnalyzePlace ap : analyzePlaces) {
+            MapPolyline polyline = new MapPolyline();
+            polyline.setTag(ap.getId());
+            polyline.setLineColor(Color.argb(128, 255, 51, 0)); // Polyline 컬러 지정.
+
+            double[][] points = ap.getPoints();
+            for (int i=0; i<5; i++){
+                polyline.addPoint(MapPoint.mapPointWithGeoCoord(points[i][1], points[i][0]));
+            }
+            mapView.addPolyline(polyline);
+        }
+    }
+
     private void setAnalyzePlaces() {
         String json = getJonString();
         jsonParsing(json);
+//        testTv.setText(analyzePlaces.get(0).getId() + "\n" + analyzePlaces.get(0).getFinalPoint());
     }
 
     private String getJonString() {
         String json = "";
         AssetManager assetManager = getAssets();
         try {
-            InputStream is = assetManager.open("0811modi.json");
+            InputStream is = assetManager.open("0811.json");
             int fileSize = is.available();
 
             byte[] buffer = new byte[fileSize];
@@ -68,15 +92,13 @@ public class OptimalPlaceActivity extends AppCompatActivity {
 
     private void jsonParsing(String json)
     {
-        double[][] points = new double[5][2]; // 좌표등록
-
         try{
             JSONObject jsonObject = new JSONObject(json);
 
-            JSONArray featureArray = jsonObject.getJSONArray("Features");
+            JSONArray featureArray = jsonObject.getJSONArray("features");
 
-//            for(int i=0; i<10; i++)
-            for(int i=0; i<featureArray.length(); i++)
+            for(int i=0; i<100; i++)
+//            for(int i=0; i<featureArray.length(); i++)
             {
                 JSONObject featureObject = featureArray.getJSONObject(i);
 
@@ -94,17 +116,18 @@ public class OptimalPlaceActivity extends AppCompatActivity {
                 String geometry = featureObject.getString("geometry");
                 JSONObject geometryObject = new JSONObject(geometry);
 
-                // sub json 2층
-                JSONArray coordinateArray = geometryObject.getJSONArray("coordinates");
-                JSONArray subArray = coordinateArray.getJSONArray(0);
-
+                // sub json 3층
+                JSONArray doubleArray = geometryObject.getJSONArray("coordinates");
+                JSONArray coordinateArray = doubleArray.getJSONArray(0);
+                double[][] coords = new double[5][];
                 for (int j = 0; j < 5; j++) {
-                    JSONArray subsubArray = subArray.getJSONArray(j);
+                    JSONArray xyArray = coordinateArray.getJSONArray(j);
+                    coords[j] = new double[2];
                     for (int k = 0; k < 2; k++) {
-                        points[j][k] = subsubArray.getDouble(k);
+                        coords[j][k] = xyArray.getDouble(k);
                     }
                 }
-                analyzePlace.setPoints(points);
+                analyzePlace.setPoints(coords);
 
                 analyzePlaces.add(analyzePlace);
             }
