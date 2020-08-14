79219c98ee4461645e8a7bec1ae17d4749d8716e
euzl <euzl@khu.ac.kr>
2020-08-14 12:20:17 +0900

    feat: 기본 레이아웃 생성
    Issue: #21

---
+++ app/src/main/java/com/dasom/khuhelper/admin/PredictActivity.java
@@ -0,0 +1,43 @@
+package com.dasom.khuhelper.admin;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.dasom.khuhelper.R;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PredictActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView predictBackBtn;
+    TextView predictNotiTv;
+    EditText numberEdt;
+    Button checkBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_optimalplace);
+
+        initView();
+    }
+
+    private void initView() {
+        predictBackBtn = findViewById(R.id.btn_predict_back);
+        predictNotiTv = findViewById(R.id.tv_predict_noti);
+        numberEdt = findViewById(R.id.edt_number);
+        checkBtn = findViewById(R.id.btn_check);
+
+        predictBackBtn.setOnClickListener(this);
+        checkBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
