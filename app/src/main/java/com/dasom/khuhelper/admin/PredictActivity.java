86e296b3748e3c917895e36036bc80eb910b5517
euzl <euzl@khu.ac.kr>
2020-08-15 05:28:59 +0900

    feat: 사용량 예측 레이아웃 생성

--- app/src/main/java/com/dasom/khuhelper/admin/PredictActivity.java
+++ app/src/main/java/com/dasom/khuhelper/admin/PredictActivity.java
@@ -15,8 +15,15 @@ public class PredictActivity extends AppCompatActivity implements View.OnClickLi
 
     ImageView predictBackBtn;
     TextView predictNotiTv;
+    TextView resultTv;
+    TextView unitTv;
     EditText numberEdt;
     Button checkBtn;
+
+    private int carNumber; // 자동차 대수
+    private int usage; // 사용량
+
+    boolean isResult = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +36,12 @@ public class PredictActivity extends AppCompatActivity implements View.OnClickLi
     private void initView() {
         predictBackBtn = findViewById(R.id.btn_predict_back);
         predictNotiTv = findViewById(R.id.tv_predict_noti);
+        resultTv = findViewById(R.id.tv_result);
+        unitTv = findViewById(R.id.tv_result_unit);
         numberEdt = findViewById(R.id.edt_number);
         checkBtn = findViewById(R.id.btn_check);
 
+        unitTv.setVisibility(View.GONE);
         predictBackBtn.setOnClickListener(this);
         checkBtn.setOnClickListener(this);
     }
@@ -43,8 +53,30 @@ public class PredictActivity extends AppCompatActivity implements View.OnClickLi
                 finish();
                 break;
             case R.id.btn_check:
-                // 어쩔까
+                if(!isResult) {
+                    showResult();
+                } else {
+                    finish();
+                }
                 break;
         }
+    }
+
+    /**
+     * 동찬 결과 보여주는 함수
+     */
+    private void showResult() {
+        numberEdt.setVisibility(View.GONE);
+
+        StringBuilder notiMessage = new StringBuilder()
+                .append("분석결과 자동차가 ")
+                .append(carNumber)
+                .append("대일 때\n예상 전기량은 다음과 같습니다.");
+        predictNotiTv.setText(notiMessage);
+
+        resultTv.setText(usage);
+        unitTv.setVisibility(View.VISIBLE);
+
+        isResult = true;
     }
 }
